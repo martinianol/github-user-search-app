416c7d4f8ba713bcfadb7d5be6b46488e8480697
Mars <martinianol@outlook.com>
2021-11-17 09:01:09 -0300

    input search dark mode

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,21 @@
 const body = document.querySelector('body');
 const mode = document.querySelector('.mode');
+const form = document.querySelector('form');
+const main = document.querySelector('main');
+const user = document.querySelector('#user')
+const summary = document.querySelector('.profile-summary');
+const icons = document.querySelectorAll('i img');
+console.log(icons);
+const placeHolder = document.querySelector('input[type=search]');
 
-console.log(body)
+console.log(placeHolder)
+
 mode.onclick = () => {
-  body.classList.toggle('dark-mode-body')
-
+  body.classList.toggle('dark-mode-body');
+  form.classList.toggle('dark-mode-component');
+  main.classList.toggle('dark-mode-component');
+  summary.classList.toggle('dark-mode-body');
+  placeHolder.style.setProperty("--c", "#fff")
+  icons.forEach(icon => icon.classList.toggle('dark-mode-icons'));
 }
 
